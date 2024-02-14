fb7acc593ac747aeec3be4132fbd87d223292766
Takashi Imahiro <imahiro@erin.jp>
2024-02-14 19:12:11 +0900

    ✨ Enable to insert tags to task field

--- js/insert_tag.js
+++ js/insert_tag.js
@@ -1,7 +1,17 @@
 window.addEventListener("hashchange", (_event) => {
   setTimeout(() => {
     initChatSendArea();
+    initTaskArea();
   }, 100);
+});
+
+document.addEventListener("click", (event) => {
+  if (
+    event.target.closest("#_taskInputInActive")?.firstChild?.id ===
+    "_taskAddArea"
+  ) {
+    initTaskArea();
+  }
 });
 
 const initChatSendArea = () => {
@@ -9,100 +19,164 @@ const initChatSendArea = () => {
     .querySelector("#_chatSendArea")
     ?.querySelector("._showDescription")?.parentNode;
   if (iconParentNode) {
-    iconParentNode.appendChild(createInfoNode(iconParentNode));
-    iconParentNode.appendChild(createInfoWithTitleNode(iconParentNode));
-    iconParentNode.appendChild(createCodeNode(iconParentNode));
-    iconParentNode.appendChild(createHrNode(iconParentNode));
+    iconParentNode.appendChild(createInfoNode(iconParentNode, "chat"));
+    iconParentNode.appendChild(createInfoWithTitleNode(iconParentNode, "chat"));
+    iconParentNode.appendChild(createCodeNode(iconParentNode, "chat"));
+    iconParentNode.appendChild(createHrNode(iconParentNode, "chat"));
   }
 };
 
-const createInfoNode = (iconParentNode) => {
-  const infoSvg = htmlStringToNode(
-    `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="16px" viewBox="0 0 24 24" width="16px" fill="#000000"><g><rect fill="none" height="24" width="24" x="0"/></g><g><g><g><path d="M4,9h16v2H4V9z M4,13h10v2H4V13z"/></g></g></g></svg>`
-  );
-  const infoNode = iconParentNode.firstChild.cloneNode(true);
-  infoNode.setAttribute("data-tooltip", "[info]");
-  infoNode.querySelector("button")?.setAttribute("id", "__tag_info");
-  infoNode.querySelector("button")?.setAttribute("aria-label", "[info]");
-  infoNode
-    .querySelector("svg")
-    ?.parentNode.replaceChild(infoSvg, infoNode.querySelector("svg"));
-  infoNode.addEventListener("mousedown", (_event) => {
-    const startTag = "[info]";
-    const endTag = "[/info]\n";
-    insertTag(startTag, endTag);
-  });
-  return infoNode;
+const initTaskArea = () => {
+  const iconParentNode = document
+    .querySelector("#_chatSendArea")
+    ?.querySelector("._showDescription")?.parentNode;
+  const taskParentNode = document.querySelector("#_taskInputActive");
+  if (iconParentNode && taskParentNode) {
+    if (taskParentNode.firstChild.id !== "__task_icon_node") {
+      const iconsNode = iconParentNode.parentNode?.cloneNode(false);
+      iconsNode.setAttribute("style", "margin-top: -8px; padding: 0px 0px 4px");
+      iconsNode.setAttribute("id", "__task_icon_node");
+      const ul = iconParentNode.cloneNode(false);
+      iconsNode.appendChild(ul);
+      iconsNode.firstChild.appendChild(createInfoNode(iconParentNode, "task"));
+      iconsNode.firstChild.appendChild(
+        createInfoWithTitleNode(iconParentNode, "task")
+      );
+      iconsNode.firstChild.appendChild(createCodeNode(iconParentNode, "task"));
+      iconsNode.firstChild.appendChild(createHrNode(iconParentNode, "task"));
+      iconsNode.firstChild.appendChild(
+        createPleaseNode(iconParentNode, "task")
+      );
+      iconsNode.firstChild.appendChild(createBowNode(iconParentNode, "task"));
+      taskParentNode.firstChild.before(iconsNode);
+    }
+  }
 };
 
-const createInfoWithTitleNode = (iconParentNode) => {
-  const infoWithTitleSvg = htmlStringToNode(
+const createInfoNode = (iconParentNode, targetType) => {
+  const svg = htmlStringToNode(
+    `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="16px" viewBox="0 0 24 24" width="16px" fill="#000000"><g><rect fill="none" height="24" width="24" x="0"/></g><g><g><g><path d="M4,9h16v2H4V9z M4,13h10v2H4V13z"/></g></g></g></svg>`
+  );
+  const node = iconParentNode.firstChild.cloneNode(true);
+  const id = `__tag_info_${targetType}`;
+  node.setAttribute("data-tooltip", "[info]");
+  node.querySelector("button")?.setAttribute("id", id);
+  node.querySelector("button")?.setAttribute("aria-label", "[info]");
+  node
+    .querySelector("svg")
+    ?.parentNode.replaceChild(svg, node.querySelector("svg"));
+  node.addEventListener("mousedown", (_event) => {
+    const startTag = "[info]";
+    const endTag = "[/info]\n";
+    insertTag(startTag, endTag, targetType);
+  });
+  return node;
+};
+
+const createInfoWithTitleNode = (iconParentNode, targetType) => {
+  const svg = htmlStringToNode(
     `<svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 0 24 24" width="16px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h12v-2H3v2zM3 6v2h18V6H3zm0 7h18v-2H3v2z"/></svg>`
   );
-  const infoWithTitleNode = iconParentNode.firstChild.cloneNode(true);
-  infoWithTitleNode.setAttribute("data-tooltip", "[info][title]");
-  infoWithTitleNode
-    .querySelector("button")
-    ?.setAttribute("id", "__tag_info_title");
-  infoWithTitleNode
-    .querySelector("button")
-    ?.setAttribute("aria-label", "[info][title]");
-  infoWithTitleNode
+  const node = iconParentNode.firstChild.cloneNode(true);
+  const id = `__tag_info_title_${targetType}`;
+  node.setAttribute("data-tooltip", "[info][title]");
+  node.querySelector("button")?.setAttribute("id", id);
+  node.querySelector("button")?.setAttribute("aria-label", "[info][title]");
+  node
     .querySelector("svg")
-    ?.parentNode.replaceChild(
-      infoWithTitleSvg,
-      infoWithTitleNode.querySelector("svg")
-    );
-  infoWithTitleNode.addEventListener("mousedown", (_event) => {
+    ?.parentNode.replaceChild(svg, node.querySelector("svg"));
+  node.addEventListener("mousedown", (_event) => {
     if (textSelected()) {
       const startTag = "[info][title][/title]";
       const endTag = "[/info]\n";
-      insertTag(startTag, endTag);
+      insertTag(startTag, endTag, targetType);
     } else {
       const startTag = "[info][title]";
       const endTag = "[/title][/info]\n";
-      insertTag(startTag, endTag);
+      insertTag(startTag, endTag, targetType);
     }
   });
-  return infoWithTitleNode;
+  return node;
 };
 
-const createCodeNode = (iconParentNode) => {
-  const codeSvg = htmlStringToNode(
+const createCodeNode = (iconParentNode, targetType) => {
+  const svg = htmlStringToNode(
     `<svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 0 24 24" width="16px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z"/></svg>`
   );
-  const codeNode = iconParentNode.firstChild.cloneNode(true);
-  codeNode.setAttribute("data-tooltip", "[code]");
-  codeNode.querySelector("button")?.setAttribute("id", "__tag_code");
-  codeNode.querySelector("button")?.setAttribute("aria-label", "[code]");
-  codeNode
+  const node = iconParentNode.firstChild.cloneNode(true);
+  const id = `__tag_code_${targetType}`;
+  node.setAttribute("data-tooltip", "[code]");
+  node.querySelector("button")?.setAttribute("id", id);
+  node.querySelector("button")?.setAttribute("aria-label", "[code]");
+  node
     .querySelector("svg")
-    ?.parentNode.replaceChild(codeSvg, codeNode.querySelector("svg"));
-  codeNode.addEventListener("mousedown", (_event) => {
+    ?.parentNode.replaceChild(svg, node.querySelector("svg"));
+  node.addEventListener("mousedown", (_event) => {
     const startTag = "[code]";
     const endTag = "[/code]\n";
-    insertTag(startTag, endTag);
+    insertTag(startTag, endTag, targetType);
   });
-  return codeNode;
+  return node;
 };
 
-const createHrNode = (iconParentNode) => {
-  const hrSvg = htmlStringToNode(
+const createHrNode = (iconParentNode, targetType) => {
+  const svg = htmlStringToNode(
     `<svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px"><path d="M160-440v-80h640v80H160Z"/></svg>`
   );
-  const hrNode = iconParentNode.firstChild.cloneNode(true);
-  hrNode.setAttribute("data-tooltip", "[hr]");
-  hrNode.querySelector("button")?.setAttribute("id", "__tag_hr");
-  hrNode.querySelector("button")?.setAttribute("aria-label", "[hr]");
-  hrNode
+  const node = iconParentNode.firstChild.cloneNode(true);
+  const id = `__tag_hr_${targetType}`;
+  node.setAttribute("data-tooltip", "[hr]");
+  node.querySelector("button")?.setAttribute("id", id);
+  node.querySelector("button")?.setAttribute("aria-label", "[hr]");
+  node
     .querySelector("svg")
-    ?.parentNode.replaceChild(hrSvg, hrNode.querySelector("svg"));
-  hrNode.addEventListener("mousedown", (_event) => {
+    ?.parentNode.replaceChild(svg, node.querySelector("svg"));
+  node.addEventListener("mousedown", (_event) => {
     const startTag = "[hr]\n";
     const endTag = "";
-    insertTag(startTag, endTag);
+    insertTag(startTag, endTag, targetType);
   });
-  return hrNode;
+  return node;
+};
+
+const createPleaseNode = (iconParentNode, targetType) => {
+  const image = htmlStringToNode(
+    `<img src="https://assets.chatwork.com/images/emoticon2x/emo_please.gif" alt="(please)" style="width: 16px; height: 16px;">`
+  );
+  const node = iconParentNode.firstChild.cloneNode(true);
+  const id = `__icon_please_${targetType}`;
+  node.setAttribute("data-tooltip", "(please)");
+  node.querySelector("button")?.setAttribute("id", id);
+  node.querySelector("button")?.setAttribute("aria-label", "(please)");
+  node
+    .querySelector("svg")
+    ?.parentNode.replaceChild(image, node.querySelector("svg"));
+  node.addEventListener("mousedown", (_event) => {
+    const startTag = "(please)\n";
+    const endTag = "";
+    insertTag(startTag, endTag, targetType);
+  });
+  return node;
+};
+
+const createBowNode = (iconParentNode, targetType) => {
+  const image = htmlStringToNode(
+    `<img src="https://assets.chatwork.com/images/emoticon2x/emo_bow.gif" alt="(bow)" style="width: 16px; height: 16px;">`
+  );
+  const node = iconParentNode.firstChild.cloneNode(true);
+  const id = `__icon_bow_${targetType}`;
+  node.setAttribute("data-tooltip", "(bow)");
+  node.querySelector("button")?.setAttribute("id", id);
+  node.querySelector("button")?.setAttribute("aria-label", "(bow)");
+  node
+    .querySelector("svg")
+    ?.parentNode.replaceChild(image, node.querySelector("svg"));
+  node.addEventListener("mousedown", (_event) => {
+    const startTag = "(bow)\n";
+    const endTag = "";
+    insertTag(startTag, endTag, targetType);
+  });
+  return node;
 };
 
 const htmlStringToNode = (str) => {
@@ -120,8 +194,11 @@ const textSelected = () => {
   }
 };
 
-const insertTag = (startTag, endTag) => {
-  const textarea = document.querySelector("#_chatText");
+const insertTag = (startTag, endTag, targetType) => {
+  const textarea =
+    targetType === "chat"
+      ? document.querySelector("#_chatText")
+      : document.querySelector("#_taskInputActive textarea");
   if (textarea) {
     const selectionStart = textarea.selectionStart;
     const selectionEnd = textarea.selectionEnd;
@@ -131,6 +208,7 @@ const insertTag = (startTag, endTag) => {
     textarea.value = before + startTag + selection + endTag + after;
     setTimeout(() => {
       textarea.focus();
+      textarea.selectionStart = selectionEnd + startTag.length;
       textarea.selectionEnd = selectionEnd + startTag.length;
     }, 100);
   }
@@ -138,4 +216,5 @@ const insertTag = (startTag, endTag) => {
 
 setTimeout(() => {
   initChatSendArea();
+  initTaskArea();
 }, 1000);
