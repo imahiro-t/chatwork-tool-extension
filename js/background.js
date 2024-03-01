2d6299f64cec70e0e6aca5bfd46a40f48eed4eac
Takashi Imahiro <imahiro@erin.jp>
2024-03-01 18:22:10 +0900

    🐛 fix to get @ mention target

--- js/background.js
+++ js/background.js
@@ -1,10 +1,43 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  const url = `https://${request.host}/gateway/init_load.php?myid=${request.myid}`;
+  const initLoadUrl = `https://${request.host}/gateway/init_load.php?myid=${request.myid}`;
+  const getAccountInfoUrl = `https://${request.host}/gateway/get_account_info.php?myid=${request.myid}`;
   const formData = new FormData();
   formData.append("pdata", JSON.stringify({ _t: request.token }));
-  fetch(url, { method: "POST", body: formData }).then((response) => {
+  fetch(initLoadUrl, { method: "POST", body: formData }).then((response) => {
     new Response(response.body).text().then((text) => {
-      sendResponse({ result: text });
+      const json = JSON.parse(text);
+      const contacts = json.result.contact_dat;
+      const contact_ids = Object.keys(contacts);
+      const room_account_ids = Object.keys(
+        Object.values(json.result.room_dat)
+          .map((x) => x.m)
+          .reduce((acc, x) => Object.assign(acc, x), {})
+      );
+      const account_ids = room_account_ids.filter(
+        (x) => !contact_ids.includes(x)
+      );
+      if (account_ids.length) {
+        const formDataForAccount = new FormData();
+        formDataForAccount.append(
+          "pdata",
+          JSON.stringify({ aid: account_ids, _t: request.token })
+        );
+        fetch(getAccountInfoUrl, {
+          method: "POST",
+          body: formDataForAccount,
+        }).then((response) => {
+          new Response(response.body).text().then((text) => {
+            const accounts = JSON.parse(text).result.account_dat;
+            sendResponse({
+              result: JSON.stringify({ ...contacts, ...accounts }),
+            });
+          });
+        });
+      } else {
+        sendResponse({
+          result: JSON.stringify({ ...contacts }),
+        });
+      }
     });
   });
   return true;
--- js/insert_tag.js
+++ js/insert_tag.js
@@ -253,7 +253,7 @@ const initContacts = () => {
       token: token,
     })
     .then((res) => {
-      contactMap = JSON.parse(res.result)["result"]["contact_dat"];
+      contactMap = JSON.parse(res.result);
     });
 };
 
@@ -489,10 +489,22 @@ const initAtMarkTo = (textarea, targetType) => {
         const s = searchWord.toLowerCase();
         const dispName = node.querySelector("p")?.textContent.toLowerCase();
         const aid = node.querySelector("img")?.getAttribute("data-aid");
-        const name = aid ? (contactMap[aid]["name"] ?? "").toLowerCase() : "";
-        const nm = aid ? (contactMap[aid]["nm"] ?? "").toLowerCase() : "";
-        const dp = aid ? (contactMap[aid]["dp"] ?? "").toLowerCase() : "";
-        const cwid = aid ? (contactMap[aid]["cwid"] ?? "").toLowerCase() : "";
+        const name =
+          aid && contactMap[aid]
+            ? (contactMap[aid]["name"] ?? "").toLowerCase()
+            : "";
+        const nm =
+          aid && contactMap[aid]
+            ? (contactMap[aid]["nm"] ?? "").toLowerCase()
+            : "";
+        const dp =
+          aid && contactMap[aid]
+            ? (contactMap[aid]["dp"] ?? "").toLowerCase()
+            : "";
+        const cwid =
+          aid && contactMap[aid]
+            ? (contactMap[aid]["cwid"] ?? "").toLowerCase()
+            : "";
         if (
           s === "" ||
           dispName.includes(s) ||
