82f52f58af44f8ae975fbb73b9dfd1ad4716e0fc
Takashi Imahiro <imahiro@erin.jp>
2024-03-19 18:03:05 +0900

    ✨ Enable to import setting from JSON

--- setting/options.js
+++ setting/options.js
@@ -21,8 +21,115 @@ document.querySelectorAll(".tab").forEach((node) => {
         contents[i].classList.remove("show");
       }
     }
+    if (index === 5) {
+      document.getElementById("save").style.display = "none";
+      document.getElementById("import").style.display = "block";
+    } else {
+      document.getElementById("save").style.display = "block";
+      document.getElementById("import").style.display = "none";
+    }
   });
 });
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+const restoreOptions = () => {
+  chrome.storage.sync.get(
+    {
+      emoji_count: "5",
+      chat_icons: ["", "", "", "", "", "", "", "", "", ""],
+      chat_messages: ["", "", "", "", "", "", "", "", "", ""],
+      chat_room_ids: ["", "", "", "", "", "", "", "", "", ""],
+      task_icons: ["", "", "", "", "", "", "", "", "", ""],
+      task_messages: ["", "", "", "", "", "", "", "", "", ""],
+      task_room_ids: ["", "", "", "", "", "", "", "", "", ""],
+      assign_icons: ["", "", "", "", "", "", "", "", "", ""],
+      assign_labels: ["", "", "", "", "", "", "", "", "", ""],
+      assign_members: ["", "", "", "", "", "", "", "", "", ""],
+      assign_room_ids: ["", "", "", "", "", "", "", "", "", ""],
+      custom_labels: [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+      ],
+      custom_messages: [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+      ],
+    },
+    (items) => {
+      document.getElementById("emoji_count").value = items.emoji_count;
+      for (i = 0; i < 10; i++) {
+        document.getElementById(`chat_icon_${i + 1}`).value =
+          items.chat_icons[i];
+        document.getElementById(`chat_message_${i + 1}`).value =
+          items.chat_messages[i];
+        document.getElementById(`chat_room_id_${i + 1}`).value =
+          items.chat_room_ids[i];
+      }
+      for (i = 0; i < 10; i++) {
+        document.getElementById(`task_icon_${i + 1}`).value =
+          items.task_icons[i];
+        document.getElementById(`task_message_${i + 1}`).value =
+          items.task_messages[i];
+        document.getElementById(`task_room_id_${i + 1}`).value =
+          items.task_room_ids[i];
+      }
+      for (i = 0; i < 10; i++) {
+        document.getElementById(`assign_icon_${i + 1}`).value =
+          items.assign_icons[i];
+        document.getElementById(`assign_label_${i + 1}`).value =
+          items.assign_labels[i];
+        document.getElementById(`assign_member_${i + 1}`).value =
+          items.assign_members[i];
+        document.getElementById(`assign_room_id_${i + 1}`).value =
+          items.assign_room_ids[i];
+      }
+      for (i = 0; i < 20; i++) {
+        document.getElementById(`custom_label_${i + 1}`).value =
+          items.custom_labels[i];
+        document.getElementById(`custom_message_${i + 1}`).value =
+          items.custom_messages[i];
+      }
+      document.getElementById("json").value = JSON.stringify(items, null, 4);
+    }
+  );
+};
 
 // Saves options to chrome.storage
 const saveOptions = () => {
@@ -146,112 +253,31 @@ const saveOptions = () => {
     () => {
       // Update status to let user know options were saved.
       const status = document.getElementById("status");
-      status.textContent = "Options saved.";
+      status.textContent = "Saved successfully.";
       setTimeout(() => {
         status.textContent = "";
+        restoreOptions();
       }, 750);
     }
   );
 };
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-const restoreOptions = () => {
-  chrome.storage.sync.get(
-    {
-      emoji_count: "5",
-      chat_icons: ["", "", "", "", "", "", "", "", "", ""],
-      chat_messages: ["", "", "", "", "", "", "", "", "", ""],
-      chat_room_ids: ["", "", "", "", "", "", "", "", "", ""],
-      task_icons: ["", "", "", "", "", "", "", "", "", ""],
-      task_messages: ["", "", "", "", "", "", "", "", "", ""],
-      task_room_ids: ["", "", "", "", "", "", "", "", "", ""],
-      assign_icons: ["", "", "", "", "", "", "", "", "", ""],
-      assign_labels: ["", "", "", "", "", "", "", "", "", ""],
-      assign_members: ["", "", "", "", "", "", "", "", "", ""],
-      assign_room_ids: ["", "", "", "", "", "", "", "", "", ""],
-      custom_labels: [
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-      ],
-      custom_messages: [
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-      ],
-    },
-    (items) => {
-      document.getElementById("emoji_count").value = items.emoji_count;
-      for (i = 0; i < 10; i++) {
-        document.getElementById(`chat_icon_${i + 1}`).value =
-          items.chat_icons[i];
-        document.getElementById(`chat_message_${i + 1}`).value =
-          items.chat_messages[i];
-        document.getElementById(`chat_room_id_${i + 1}`).value =
-          items.chat_room_ids[i];
-      }
-      for (i = 0; i < 10; i++) {
-        document.getElementById(`task_icon_${i + 1}`).value =
-          items.task_icons[i];
-        document.getElementById(`task_message_${i + 1}`).value =
-          items.task_messages[i];
-        document.getElementById(`task_room_id_${i + 1}`).value =
-          items.task_room_ids[i];
-      }
-      for (i = 0; i < 10; i++) {
-        document.getElementById(`assign_icon_${i + 1}`).value =
-          items.assign_icons[i];
-        document.getElementById(`assign_label_${i + 1}`).value =
-          items.assign_labels[i];
-        document.getElementById(`assign_member_${i + 1}`).value =
-          items.assign_members[i];
-        document.getElementById(`assign_room_id_${i + 1}`).value =
-          items.assign_room_ids[i];
-      }
-      for (i = 0; i < 20; i++) {
-        document.getElementById(`custom_label_${i + 1}`).value =
-          items.custom_labels[i];
-        document.getElementById(`custom_message_${i + 1}`).value =
-          items.custom_messages[i];
-      }
-    }
-  );
+// Import options to chrome.storage
+const importOptions = () => {
+  const json = document.getElementById("json").value;
+  const items = JSON.parse(json);
+  console.log(items);
+  chrome.storage.sync.set(items, () => {
+    // Update status to let user know options were saved.
+    const status = document.getElementById("status");
+    status.textContent = "Imported successfully.";
+    setTimeout(() => {
+      status.textContent = "";
+      restoreOptions();
+    }, 750);
+  });
 };
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.getElementById("save").addEventListener("click", saveOptions);
+document.getElementById("import").addEventListener("click", importOptions);
